e1313cf90c75300a931c86487aac0a828da669f4
KEDARNAHIRE <kedarnahire.177@gmail.com>
2025-07-27 12:04:07 +0530

    DataTypes

--- DataTypes/ClassCode/Program1.java
+++ DataTypes/ClassCode/Program1.java
@@ -2,7 +2,7 @@ package DataTypes;
 class Program1{
     public static void main(String[] args){
         //Byte 1 byte
-        //Range -128 to 127
+        //Range -128 - 127
         byte x = (byte)128;
         System.out.println(x); 
     }
